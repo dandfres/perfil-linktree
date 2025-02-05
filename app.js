26e6d51418339d923665cd2d43fb1941b9694cc0
Sebastian <sebas00tiam@gmail.com>
2025-02-05 16:25:40 -0500

    Proyecto terminado

---
+++ app.js
@@ -0,0 +1,6 @@
+
+const h2Element = document.querySelector(".profile h2");
+h2Element.textContent = "Sebastian Durand";
+
+const pElement = document.querySelector(".profile p");
+pElement.textContent = "Estudiante de Desarrollo de Software Full Stack";
